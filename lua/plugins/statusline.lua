44b7da7c6366e029b1df81d87372df44e3644dc6
Danijel Milosevic <contact@danijel.dev>
2025-03-26 15:22:06 +0100

    Fix lualine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -6,7 +6,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "dracula",
+				theme = "auto",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
@@ -15,32 +15,40 @@ return {
 				},
 				ignore_focus = {},
 				always_divide_middle = true,
-				globalstatus = false,
+				always_show_tabline = false,
+				globalstatus = true,
 				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+					statusline = 100,
+					tabline = 100,
+					winbar = 100,
 				},
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						require("nvim-possession").status,
+						cond = function()
+							return require("nvim-possession").status() ~= nil
+						end,
+					},
+					{ "buffers" },
+				},
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
+				lualine_a = { "filename" },
+				lualine_b = { "location" },
 			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
+			tabline = {
+				lualine_a = { { "tabs", mode = 2 } },
+			},
+			winbar = {
+				lualine_a = { "filename" },
+			},
 			extensions = {},
 		})
 	end,
