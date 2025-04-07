dff04cbc1ee812a65cb2d8d128d8614f6bd84023
Danijel Milosevic <contact@danijel.dev>
2025-04-07 22:27:28 +0200

    Staging place for new vim-sleuth

---
+++ lua/plugins/tabspace.lua
@@ -0,0 +1,8 @@
+-- Automatically detect the indentation style used in a buffer and updating the buffer options accordingly.
+-- Other plugins with same function:
+-- https://github.com/Darazaki/indent-o-matic
+-- https://github.com/tpope/vim-sleuth
+return {
+	"nmac427/guess-indent.nvim",
+	opts = {},
+}
