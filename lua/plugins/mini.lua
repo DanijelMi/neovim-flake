cce32d6ee6c0a6741e2ac8130334f073a7ad9784
Danijel Milosevic <contact@danijel.dev>
2025-04-07 15:17:44 +0200

    Move surround to mini bundle

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,6 +5,8 @@ return {
 		config = function()
 			-- Notification GUI top right
 			vim.notify = require("mini.notify").make_notify()
+			-- surround objects with '"({[ etc
+			require("mini.surround").setup()
 		end,
 	},
 }
