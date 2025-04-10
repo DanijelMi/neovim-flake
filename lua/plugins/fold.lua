7cd2d636a61fc496dd406a6040c1f7bcdebbaaa7
Danijel Milosevic <contact@danijel.dev>
2025-04-10 12:38:46 +0200

    Update folding to be less intrusive

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -1,32 +1,41 @@
+-- Stripped down config from here https://github.com/kevinhwang91/nvim-ufo/issues/4
 return {
 	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
-	config = function()
-		-- vim.o.foldcolumn = "1" -- '0' is not bad
-		vim.o.foldcolumn = "auto:9"
+	dependencies = "kevinhwang91/promise-async",
+	event = "VeryLazy",
+	opts = {
+		-- INFO: Uncomment to use treeitter as fold provider, otherwise nvim lsp is used
+		provider_selector = function(bufnr, filetype, buftype)
+			return { "treesitter", "indent" }
+		end,
+		open_fold_hl_timeout = 400,
+		close_fold_kinds_for_ft = { "imports", "comment" },
+		preview = {
+			win_config = {
+				border = { "", "─", "", "", "", "─", "", "" },
+				winhighlight = "Normal:Folded",
+				winblend = 0,
+			},
+		},
+	},
+	init = function()
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.o.foldcolumn = "1" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
-
-		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+	end,
+	config = function(_, opts)
+		require("ufo").setup()
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
-		-- Option 2: nvim lsp as LSP client
-		-- Tell the server the capability of foldingRange,
-		-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.foldingRange = {
-			dynamicRegistration = false,
-			lineFoldingOnly = true,
-		}
-		local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-		for _, ls in ipairs(language_servers) do
-			require("lspconfig")[ls].setup({
-				capabilities = capabilities,
-				-- you can add other fields for setting up lsp server in this table
-			})
-		end
-		require("ufo").setup()
+		vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				-- vim.lsp.buf.hover()
+				vim.cmd([[ Lspsaga hover_doc ]])
+			end
+		end)
 	end,
 }
