05f604b3e1d8ca6d88ff87c6cd7481d1e88da596
Danijel Milosevic <contact@danijel.dev>
2025-04-10 12:40:41 +0200

    Unlock full width for tabline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -53,7 +53,7 @@ return {
 				lualine_b = { "location" },
 			},
 			tabline = {
-				lualine_a = { { "tabs", mode = 2 } },
+				lualine_a = { { "tabs", max_length = vim.o.columns, mode = 2 } },
 			},
 			winbar = {
 				lualine_a = { "filename" },
