38f6e05edbf6d76127acea75888ca901424cbb09
Danijel Milosevic <contact@danijel.dev>
2025-04-13 13:26:51 +0200

    retab

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,52 +1,53 @@
 return {
-  -- Render markdown inside the editor
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    main = "render-markdown",
-    opts = { sign = { enabled = false } },
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  },
+	-- Render markdown inside the editor
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		enabled = true,
+		main = "render-markdown",
+		opts = { sign = { enabled = false } },
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	},
 
-  -- Render images inside the editor
-  {
-    "3rd/image.nvim",
-    enabled = false,
-    dependencies = {
-      -- "leafo/magick",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    -- TODO Wrap this entire invocation to only work if a "magick" binary exists on the host OS
-    config = function()
-      local configs = require("image")
-      configs.setup({
-        backend = "kitty",
-        integrations = {
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = true,
-            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-          },
-          neorg = {
-            enabled = false,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "norg" },
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false,                                 -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        editor_only_render_when_focused = false,                              -- auto show/hide images when the editor gains/looses focus
-        tmux_show_only_in_active_window = false,                              -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-      })
-    end,
-  },
+	-- Render images inside the editor
+	{
+		"3rd/image.nvim",
+		enabled = false,
+		dependencies = {
+			-- "leafo/magick",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		-- TODO Wrap this entire invocation to only work if a "magick" binary exists on the host OS
+		config = function()
+			local configs = require("image")
+			configs.setup({
+				backend = "kitty",
+				integrations = {
+					markdown = {
+						enabled = true,
+						clear_in_insert_mode = false,
+						download_remote_images = true,
+						only_render_image_at_cursor = true,
+						filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+					},
+					neorg = {
+						enabled = false,
+						clear_in_insert_mode = false,
+						download_remote_images = true,
+						only_render_image_at_cursor = false,
+						filetypes = { "norg" },
+					},
+				},
+				max_width = nil,
+				max_height = nil,
+				max_width_window_percentage = nil,
+				max_height_window_percentage = 50,
+				window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+				window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+				editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+				tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+				hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+			})
+		end,
+	},
 }
