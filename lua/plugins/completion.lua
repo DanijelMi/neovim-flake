7a3a8c7c22fb73933238fc1b1dffbdc3078d4ad6
Danijel Milosevic <contact@danijel.dev>
2025-04-14 13:34:11 +0200

    LSP overhaul

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -131,10 +131,13 @@ return {
 					ghost_text = { enabled = true },
 				},
 				-- Experimental signature help support
-				signature = { enabled = true, trigger = {
+				signature = {
 					enabled = true,
-					show_on_keyword = true,
-				} },
+					trigger = {
+						enabled = true,
+						show_on_keyword = true,
+					},
+				},
 				fuzzy = {
 					implementation = "prefer_rust_with_warning",
 				},
@@ -154,6 +157,25 @@ return {
 						sql = { "snippets", "dadbod", "buffer" },
 					},
 					providers = {
+						lsp = {
+							name = "LSP",
+							module = "blink.cmp.sources.lsp",
+							opts = {}, -- Passed to the source directly, varies by source
+							--- NOTE: All of these options may be functions to get dynamic behavior
+							--- See the type definitions for more information
+							enabled = true, -- Whether or not to enable the provider
+							async = false, -- Whether we should show the completions before this provider returns, without waiting for it
+							timeout_ms = 1000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
+							transform_items = nil, -- Function to transform the items before they're returned
+							should_show_items = true, -- Whether or not to show the items
+							max_items = nil, -- Maximum number of items to display in the menu
+							min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
+							-- If this provider returns 0 items, it will fallback to these providers.
+							-- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+							fallbacks = {},
+							score_offset = 30, -- Boost/penalize the score of the items
+							override = nil, -- Override the source's functions
+						},
 						lazydev = {
 							name = "LazyDev",
 							module = "lazydev.integrations.blink",
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,71 +14,9 @@ return {
 	{
 		-- Quickstart configs for Nvim LSP
 		"neovim/nvim-lspconfig",
-		keys = {
-			-- {
-			-- 	"<leader>lr",
-			-- 	vim.lsp.buf.rename,
-			-- 	desc = "LSP Rename",
-			-- },
-			-- {
-			-- 	"<leader>lc",
-			-- 	vim.lsp.buf.code_action,
-			-- 	desc = "LSP Code Action",
-			-- },
-			-- {
-			-- 	"<leader>ld",
-			-- 	vim.lsp.buf.declaration,
-			-- 	desc = "LSP Goto Declaration",
-			-- },
-		},
+		dependencies = "saghen/blink.cmp",
+		-- config = function(_, opts)
 		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-				callback = function(event)
-					-- Create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
-				end,
-			})
-
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP specification.
-			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
 			-- capabilities.textDocument.foldingRange = {
 			--   dynamicRegistration = false,
 			--   lineFoldingOnly = true,
@@ -93,46 +31,24 @@ return {
 			-- })
 
 			local lspconfig = require("lspconfig")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			-- Lua LSP
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						telemetry = { enable = false },
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
+			-- Lua
+			lspconfig["lua_ls"].setup({
 				capabilities = capabilities,
+				settings = { Lua = { runtime = { version = "LuaJIT" } } },
 			})
-
-			-- Nix LSP
-			lspconfig.nixd.setup({
-				-- on_attach = on_attach(),
-				capabilities = capabilities,
-			})
-
-			-- Markdown LSP
-			lspconfig.marksman.setup({
-				settings = {
-					single_file_support = false,
-				},
-				capabilities = capabilities,
-			})
-
-			-- Terraform LSP
-			lspconfig.terraformls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tflint.setup({
-				capabilities = capabilities,
-			})
+			-- Nix
+			lspconfig["nixd"].setup({ capabilities = capabilities })
+			-- Markdown
+			lspconfig["marksman"].setup({ capabilities = capabilities })
+			-- Terraform
+			lspconfig["terraformls"].setup({ capabilities = capabilities })
+			lspconfig["tflint"].setup({ capabilities = capabilities })
+			-- Bash
+			lspconfig["bashls"].setup({ capabilities = capabilities })
 
 			-- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "LSP: Show diagnostic" })
-			-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "LSP: goto prev" })
-			-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "LSP: goto next" })
 			-- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { desc = "LSP: setloclist" })
 			-- vim.keymap.set("n", "<space>k", vim.lsp.buf.hover, { desc = "LSP: hover" })
 			-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
