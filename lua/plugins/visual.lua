581459fee4c1653c5e4f77e50f448418ba1536a6
Danijel Milosevic <contact@danijel.dev>
2025-04-26 18:21:46 +0200

    Add auto-highlight all instances of selected word

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -149,4 +149,18 @@ return {
 			})
 		end,
 	},
+	{
+		"RRethy/vim-illuminate",
+		config = function()
+			require("illuminate").configure({
+				providers = {
+					"lsp",
+					"treesitter",
+					"regex",
+				},
+				delay = 200,
+				under_cursor = true,
+			})
+		end,
+	},
 }
