a5c9690aa80bb3f4a56d28cfb665463906550544
Danijel Milosevic <contact@danijel.dev>
2025-10-14 12:29:08 +0200

    refactor(ai): new version format changes

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -98,24 +98,23 @@ return {
 					},
 				},
 				adapters = {
-					opts = {
-						show_defaults = false, -- Remove builtin adapters
-					},
-					openrouter = function()
-						return require("codecompanion.adapters").extend("openai_compatible", {
-							name = "OpenRouter",
-							env = {
-								url = "https://openrouter.ai/api",
-								api_key = os.getenv("OPENROUTER_API_KEY"),
-								chat_url = "/v1/chat/completions",
-							},
-							schema = {
-								model = {
-									default = current_model,
+					http = {
+						openrouter = function()
+							return require("codecompanion.adapters").extend("openai_compatible", {
+								name = "OpenRouter",
+								env = {
+									url = "https://openrouter.ai/api",
+									api_key = os.getenv("OPENROUTER_API_KEY"),
+									chat_url = "/v1/chat/completions",
 								},
-							},
-						})
-					end,
+								schema = {
+									model = {
+										default = current_model,
+									},
+								},
+							})
+						end,
+					}
 				},
 				extensions = {
 					mcphub = {
